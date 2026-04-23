4db18e812ed77d78cc167ff497e4ed6a9e213a0b
guiliwu <guiliwu74@163.com>
2026-04-23 22:25:39 +0800

    Configure GitHub Pages: set base path and BrowserRouter basename

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/scheduling-app">
         <App />
       </BrowserRouter>
     </AuthProvider>
